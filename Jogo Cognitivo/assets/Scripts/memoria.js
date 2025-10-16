99635755d4d1a395043a93949077d1c6b48cd1d4
Gabriela Zuquete <zuquetegabriela@gmail.com>
2025-10-16 20:08:57 +0000

    atualizando jogo da memoria

--- Jogo Cognitivo/assets/Scripts/memoria.js
+++ Jogo Cognitivo/assets/Scripts/memoria.js
@@ -1,16 +1,10 @@
 addEventListener("DOMContentLoaded", () => {
-    const tabuleiro = document.querySelector("#cartas")
-    const mapa = [
-        0,0,0,
-        0,0,0,
-        0,0,0
-    ]
+  const tabuleiro = document.querySelector("#cartas");
+  const mapa = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-    for (let i = 0; i < 12; i++) {
-
-        tabuleiro.innerHTML += `<input id="${i}" type="button" class="${mapa[i] == 0 ? "parede" : "parede"
-            }">`
-    }
-  
-})
-
+  for (let i = 0; i < 12; i++) {
+    tabuleiro.innerHTML += `<input id="${i}" type="button" class="${
+      mapa[i] == 0 ? "parede" : "parede"
+    }">`;
+  }
+});
